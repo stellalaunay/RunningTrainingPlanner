358f157bab252243d21ec58df27af80bdec98d99
stellalaunay <slaunay2@illinois.edu>
2026-07-28 15:31:54 -0700

    Initial Commit

---
+++ RunningTrainingPlanner/RunningTrainingPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningTrainingPlannerApp.swift
+//  RunningTrainingPlanner
+//
+//  Created by Stella Launay on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RunningTrainingPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunningTrainingPlannerTests/RunningTrainingPlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  RunningTrainingPlannerTests.swift
+//  RunningTrainingPlannerTests
+//
+//  Created by Stella Launay on 7/28/26.
+//
+
+import Testing
+@testable import RunningTrainingPlanner
+
+struct RunningTrainingPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
