0336e1f17fb99716bc053cec0b8299539ed392e5
Stella Launay <stellalaunay6@gmail.com>
2026-08-18 16:35:01 -0700

    Resolve firebase initialization error, don't reinitialize of already exists

--- backend/src/auth/firebase.py
+++ backend/src/auth/firebase.py
@@ -2,5 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 def init_firebase() -> None:
+    if firebase_admin._apps:
+        return
     cred = credentials.Certificate("secrets/firebase-service-account.json")
     firebase_admin.initialize_app(cred)
