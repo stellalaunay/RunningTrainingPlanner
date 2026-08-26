f8e63106f9232950cc8f6aec0ad01ac06c1dc662
Stella Launay <stellalaunay6@gmail.com>
2026-08-26 12:44:22 -0700

    Load Firebase credentials from env variable

--- backend/src/auth/firebase.py
+++ backend/src/auth/firebase.py
@@ -1,8 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
+import base64
+import json
 
 def init_firebase() -> None:
     if firebase_admin._apps:
         return
-    cred = credentials.Certificate("secrets/firebase-service-account.json")
+    encoded = os.getenv("FIREBASE_CREDENTIALS")
+    cred_dict = json.loads(base64.b64decode(encoded))
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
